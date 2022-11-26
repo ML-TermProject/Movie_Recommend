399a377140a892a8ca60e46ef0feee3c86296ad3
minkyung <minkyung7365@gmail.com>
2022-11-26 16:03:17 +0900

    content 코드 합침

--- MovieRecommend.py
+++ MovieRecommend.py
@@ -6,11 +6,198 @@ from sklearn.neighbors import NearestNeighbors
 from surprise.dataset import DatasetAutoFolds, Reader
 from surprise import BaselineOnly, SVD
 from surprise.model_selection import cross_validate
+from ast import literal_eval
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
+from nltk.stem.snowball import SnowballStemmer
 import warnings
 warnings.filterwarnings(action='ignore')
 
 pd.options.display.max_columns = None
 pd.options.display.width = None
+
+### For Content-Based
+def read_file():
+    links_small = pd.read_csv('./input/links_small.csv')
+    links_small = links_small[links_small['tmdbId'].notnull()]['tmdbId'].astype('int')
+
+    md = pd.read_csv('./input/movies_metadata.csv')
+    md['genres'] = md['genres'].fillna('[]').apply(literal_eval).apply(
+        lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    md['year'] = pd.to_datetime(md['release_date'], errors='coerce').apply(
+        lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+    md = md.drop([19730, 29503, 35587])
+    md['id'] = md['id'].astype('int')
+
+    smd = md[md['id'].isin(links_small)]
+
+    return links_small, md, smd
+
+
+# =========Movie Description Based Recommender=========
+def description_based(smd):
+    smd['tagline'] = smd['tagline'].fillna('')
+    smd['description'] = smd['overview'] + smd['tagline']
+    smd['description'] = smd['description'].fillna('')
+
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(smd['description'])
+
+    # Cosine Similarity
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    smd = smd.reset_index()
+    titles = smd['title']
+    indices = pd.Series(smd.index, index=smd['title'])
+
+    return titles, indices, cosine_sim
+
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:31]
+    movie_indices = [i[0] for i in sim_scores]
+    return titles.iloc[movie_indices]
+
+
+links_small, md, smd = read_file()
+titles, indices, cosine_sim = description_based(smd)
+
+print("=========Movie Description Based Recommender=========")
+print(get_recommendations('Mean Girls'))
+print("=====================================================\n")
+
+
+# =========Metadata Based Recommender=========
+# 위의 추천 시스템은 단순히 Overview, Tagline만 보고 추천하는 것이기 때문에 별로임
+# 그래서 credit과 keyword를 column에 추가 (-> md.merge)
+
+def meta_based(md,smd):
+    credits = pd.read_csv('./input/credits.csv')
+    keywords = pd.read_csv('./input/keywords.csv')
+
+    keywords['id'] = keywords['id'].astype('int')
+    credits['id'] = credits['id'].astype('int')
+    md['id'] = md['id'].astype('int')
+
+    md = md.merge(credits, on='id')
+    md = md.merge(keywords, on='id')
+
+    smd = md[md['id'].isin(links_small)]
+
+    smd['cast'] = smd['cast'].apply(literal_eval)
+    smd['crew'] = smd['crew'].apply(literal_eval)
+    smd['keywords'] = smd['keywords'].apply(literal_eval)
+    smd['cast_size'] = smd['cast'].apply(lambda x: len(x))
+    smd['crew_size'] = smd['crew'].apply(lambda x: len(x))
+
+    return md, smd
+
+
+md, smd = meta_based(md, smd)
+
+def get_director(x):
+    for i in x:
+        if i['job'] == 'Director':
+            return i['name']
+    return np.nan
+
+
+def modify_smd(smd):
+    smd['director'] = smd['crew'].apply(get_director)
+    smd['cast'] = smd['cast'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    smd['cast'] = smd['cast'].apply(lambda x: x[:3] if len(x) >= 3 else x)
+    smd['keywords'] = smd['keywords'].apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+    smd['cast'] = smd['cast'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+    smd['director'] = smd['director'].astype('str').apply(lambda x: str.lower(x.replace(" ", "")))
+    smd['director'] = smd['director'].apply(lambda x: [x, x, x])
+
+    s = smd.apply(lambda x: pd.Series(x['keywords']), axis=1).stack().reset_index(level=1, drop=True)
+    s.name = 'keyword'
+    s = s.value_counts()
+    s = s[s > 1]  # 한번도 사용되지 않은 키워드는 삭제
+
+    stemmer = SnowballStemmer('english')
+    # stemmer.stem('dogs')  # 그냥 dog로 출력하게 함 (다른 단어들도 마찬가지, 다 기본형으로)
+
+    return s, smd, stemmer
+
+
+s, smd, stemmer = modify_smd(smd)
+
+
+def filter_keywords(x):
+    words = []
+    for i in x:
+        if i in s:
+            words.append(i)
+    return words
+
+def modify_smd2(smd):
+    smd['keywords'] = smd['keywords'].apply(filter_keywords)
+    smd['keywords'] = smd['keywords'].apply(lambda x: [stemmer.stem(i) for i in x])
+    smd['keywords'] = smd['keywords'].apply(lambda x: [str.lower(i.replace(" ", "")) for i in x])
+
+    smd['soup'] = smd['keywords'] + smd['cast'] + smd['director'] + smd['genres']
+    smd['soup'] = smd['soup'].apply(lambda x: ' '.join(x))
+
+    count = CountVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0, stop_words='english')
+    count_matrix = count.fit_transform(smd['soup'])
+
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+    return smd, count, count_matrix, cosine_sim
+
+
+smd, count, count_matrix, cosine_sim = modify_smd2(smd)
+
+
+def popularityNratings(md):
+    vote_counts = md[md['vote_count'].notnull()]['vote_count'].astype('int')
+    vote_averages = md[md['vote_average'].notnull()]['vote_average'].astype('int')
+    m = vote_counts.quantile(0.95)
+    C = vote_averages.mean()
+
+    return m, C
+
+
+m, C = popularityNratings(md)
+
+
+def weighted_rating(x):
+    v = x['vote_count']
+    R = x['vote_average']
+    return (v/(v+m) * R) + (m/(m+v) * C)
+
+
+def improved_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:26]
+    movie_indices = [i[0] for i in sim_scores]
+
+    movies = smd.iloc[movie_indices][['title', 'vote_count', 'vote_average', 'year']]
+    vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
+    vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('int')
+    C = vote_averages.mean()
+    m = vote_counts.quantile(0.60)
+    qualified = movies[(movies['vote_count'] >= m) & (movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
+    qualified['vote_count'] = qualified['vote_count'].astype('int')
+    qualified['vote_average'] = qualified['vote_average'].astype('int')
+    qualified['wr'] = qualified.apply(weighted_rating, axis=1)
+    qualified = qualified.sort_values('wr', ascending=False).head(10)
+    return qualified
+
+
+print("=============Metadata Based Recommender=============")
+print(improved_recommendations('Mean Girls'))
+print("=====================================================\n")
+
+
+
 
 ### For Item-Based
 # Get the closest match with the input movie
@@ -218,12 +405,12 @@ def user_based(df_credit, df_rating, algo, userId):
 # Read dataset for collaborative filtering (item/user-based)
 def read_file():
     # read csv files
-    metadata = pd.read_csv("./movies_metadata.csv",
+    metadata = pd.read_csv("./input/movies_metadata.csv",
                            usecols=['id', 'imdb_id', 'original_title'],
                            dtype={'id': 'str', 'imdb': 'str', 'original_title': 'str'})
-    link = pd.read_csv("./links_small.csv",
+    link = pd.read_csv("./input/links_small.csv",
                        usecols=['movieId', 'imdbId', 'tmdbId'])
-    rating = pd.read_csv("./ratings_small.csv",
+    rating = pd.read_csv("./input/ratings_small.csv",
                          usecols=['userId', 'movieId', 'rating'],
                          dtype={'userId': 'int32', 'movieId': 'int32', 'rating': 'float32'})
 
@@ -248,6 +435,9 @@ def read_file():
 credit, rating = read_file()  # Read dataset for Collaborative Filtering
 
 ### Test
+# Content-based Filtering
+
+
 # Collaborative Filtering (item-based)
 item_based(credit, rating, "Iron Man")
 print("-------------------------------------------------------------------\n")
